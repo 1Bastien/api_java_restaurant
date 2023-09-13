b682929101dfae96c31c623d552c1bfdbd70a72c
Popeye <bastien.brgt@protonmail.com>
2023-09-13 23:39:55 +0200

    Changement nom package

--- src/main/java/com/restaurant/api/ApiRestaurant.java
+++ src/main/java/com/restaurant/api/ApiRestaurant.java
@@ -1,13 +1,13 @@
-package com.example.api;
+package com.restaurant.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiApplication {
+public class ApiRestaurant {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
+		SpringApplication.run(ApiRestaurant.class, args);
 	}
 
 }
