541a9f14349e806faaa285bbee69394860a3d1f3
Popeye <bastien.brgt@protonmail.com>
2023-09-18 17:29:31 +0200

    ajout methodes de recherche en fonction du restaurant et des dates

--- src/main/java/com/restaurant/api/repository/BookingRepository.java
+++ src/main/java/com/restaurant/api/repository/BookingRepository.java
@@ -1,11 +1,26 @@
 package com.restaurant.api.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.restaurant.api.model.Booking;
+import com.restaurant.api.model.Restaurant;
 
 @Repository
-public interface BookingRepository extends CrudRepository<Booking, Long> {
-
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+	
+	@Query("SELECT b FROM Booking b WHERE b.date BETWEEN :startDate AND :endDate AND b.restaurant = :restaurant")
+    List<Booking> findBookingsBetweenDate(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate, @Param("restaurant") Restaurant restaurant);
+	
+	List<Booking> findByRestaurantId(Long restaurantId);
 }
+
+
+
+    
+
