8871204630077b0bf63221ebc3e510d4faa7600f
Popeye <bastien.brgt@protonmail.com>
2023-09-18 17:29:54 +0200

    Ajout methode de recherche par nom

--- src/main/java/com/restaurant/api/repository/RestaurantRepository.java
+++ src/main/java/com/restaurant/api/repository/RestaurantRepository.java
@@ -1,11 +1,15 @@
 package com.restaurant.api.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.restaurant.api.model.Restaurant;
 
 @Repository
-public interface RestaurantRepository extends CrudRepository<Restaurant, Long> {
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+	Optional<Restaurant> findByName(String name);
 
 }
